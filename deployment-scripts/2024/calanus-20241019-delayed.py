351b3d9b85555559940b4cf9f7a7ba496afbfb32
Sam Woodman <sam.woodman@noaa.gov>
2026-09-22 19:12:29 +0000

    batch update institution to ROR name

--- deployment-scripts/2024/calanus-20241019-delayed.py
+++ deployment-scripts/2024/calanus-20241019-delayed.py
@@ -5,7 +5,7 @@ import numpy as np
 import xarray as xr
 from esdglider.slocum import pipeline
 
-from esdglider import aa, gcp, paths, plots, qartod
+from esdglider import aa, gcp, paths, plots, qartod, utils
 
 logger = logging.getLogger(__name__)
 
@@ -164,12 +164,12 @@ if __name__ == "__main__":
     
     #--------------------------------------------------------------------------
     ### Generate profile netCDF files for the DAC
-    # glider.ngdac_profiles(
-    #     outname_dict["outname_tssci"], 
-    #     paths['profdir'], 
-    #     paths['deploymentyaml'],
-    #     force=True, 
-    # )
+    utils.create_ngdac_profiles(
+        inname=outname_dict["outname_tssci"],
+        outdir=glider_paths["ngdacdir"],
+        deploymentyaml=glider_paths["deploymentyaml"],
+        force=True,
+    )
 
     #--------------------------------------------------------------------------
     logger.info("Completed scheduled processing")
